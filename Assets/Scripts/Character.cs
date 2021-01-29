18bf48cb1cf7d7fa0ba2ea52d575d16cb8cbbcfa
Hanrui Zhang <71097187+Henry9708@users.noreply.github.com>
2021-01-29 13:29:39 -0500

    background color changeable

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float upspeed;
+
+    public Transform MovePosition;
+
+    private Rigidbody2D Rig;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Rig.velocity = new Vector3(speed * horizontal, Rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rig.AddForce(Vector3.up * upspeed, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+
+    Camera cm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = GetComponent<Camera>();
+        cm.backgroundColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(cm.backgroundColor == Color.white)
+            {
+                cm.backgroundColor = Color.black;
+            }
+            else
+            {
+                cm.backgroundColor = Color.white;
+            }
+        }
+    }
+}
