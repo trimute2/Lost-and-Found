2735ef24fc8220ad4062701e19524a5600f16c02
trimute2 <ben.j.stern@gmail.com>
2021-01-29 20:39:35 -0500

    added pressure plate and level end

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class LevelExit : MonoBehaviour
+{
+	public string NextLevelName;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.GetComponent<Character>() != null)
+		{
+			SceneManager.LoadScene(NextLevelName);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PressurePlateScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using System;
+
+[Serializable]
+public class PressurePlateEvent : UnityEvent<bool> { };
+
+[RequireComponent(typeof(Collider2D))]
+public class PressurePlateScript : MonoBehaviour
+{
+
+	public PressurePlateEvent OnPressureplateEvent;
+
+	private Collider2D collider;
+	private int touchCount;
+
+	private void Start()
+	{
+		collider = GetComponent<Collider2D>();
+		touchCount = 0;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		touchCount++;
+		TestTrigger();
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		touchCount--;
+		TestTrigger();
+	}
+
+	private void OnEnable()
+	{
+		TestTrigger();
+	}
+
+	private void OnDisable()
+	{
+		OnPressureplateEvent.Invoke(false);
+	}
+
+	private void TestTrigger()
+	{
+		if (enabled)
+		{
+			if (touchCount > 0)
+			{
+				OnPressureplateEvent.Invoke(true);
+			}
+			else
+			{
+				OnPressureplateEvent.Invoke(false);
+			}
+		}
+	}
+
+
+}
